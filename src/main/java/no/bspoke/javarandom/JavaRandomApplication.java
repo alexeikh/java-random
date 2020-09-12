5ec622b562344a254070ec285b159565f8e28be9
Alexei Khlebnikov <alexei.khlebnikov@gmail.com>
2020-09-12 19:52:12 +0200

    Added simple java-random project.

---
+++ src/main/java/no/bspoke/javarandom/JavaRandomApplication.java
@@ -0,0 +1,13 @@
+package no.bspoke.javarandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRandomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRandomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/bspoke/javarandom/JavaRandomApplicationTests.java
@@ -0,0 +1,13 @@
+package no.bspoke.javarandom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRandomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
